b82bf2cb1a965084fb2b4f2eedc7bc79b93dbaa6
sKawashima <bysk.333@gmail.com>
2023-11-07 13:30:41 +0900

    Update model

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { OpenAI } from 'openai';
 import readline from 'readline';
 import dotenv from 'dotenv';
+import { highlight } from'cli-highlight';
 
 dotenv.config();
 
@@ -21,9 +22,10 @@ You are an excellent and friendly engineering supporter.
 async function generateCode(prompt: string) {
   // OpenAIのAPIを使用してコードを生成
   const response = await openai.chat.completions.create({
-    model: "gpt-4",
+    model: "gpt-4-1106-preview",
     messages: [{role: 'system', content: SYSTEM_PROMPT },{role: 'user', content: prompt }],
     temperature: 0.1,
+    response_format: { type: "json_object" },
   });
 
   // 生成されたコードを返す
@@ -75,7 +77,7 @@ Please output the source code in an executable language, with supplementary expl
     const terminalOutput = `
 SYSTEM> コード：
 
-${code}
+${highlight(code)}
 
 SYSTEM> ${advice}
 
